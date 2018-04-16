c82cc9ea00fa89766f7c1fb67a67bad06d77b26c
Jacob O'Donnell <jacobodonnell@gmail.com>
2018-04-15 21:37:46 -0400

    npm install if its not been installed yet

--- add-js-import.el
+++ add-js-import.el
@@ -15,6 +15,8 @@
 ;; put import at bottom of import list
 ;; handle more than one matches
 ;; make install plan
+;; check if file is already being imported from
+;; requires projectile
 
 (require 'seq)
 (require 'thingatpt)
@@ -29,45 +31,64 @@
   :type '(choice (const nil) string)
   :group 'add-js-import)
 
+(defcustom add-js-import-npm-executable "npm"
+  "Name or path of the npm executable binary file."
+  :type '(choice (const nil) string)
+  :group 'add-js-import)
+
 (defvar add-js-import-node-import-path
   (concat (file-name-directory buffer-file-name) "find_js_import.js"))
 
-(defun is-empty? (thing)
+(defun add-js-import--is-empty? (thing)
   (eq (length thing) 0))
 
-(defun command-list(project-path symbol)
-  (if use-semicolons
-      `(,add-js-import-node-executable
-        ,add-js-import-node-import-path
-        ,project-path
-        ,symbol
-        "--semi")
-    `(,add-js-import-node-executable
-      ,add-js-import-node-import-path
-      ,project-path
-      ,symbol)))
+(defun add-js-import--semi-flag()
+  (if add-js-import-use-semicolons
+      "--semi"
+    ""))
 
-(defun run-command(project-path symbol)
+(defun add-js-import--command-list(project-path symbol)
+  `(,add-js-import-node-executable
+    ,add-js-import-node-import-path
+    ,project-path
+    ,symbol
+    ,(add-js-import--semi-flag)))
+
+(defun add-js-import--run-command(project-path symbol)
   (shell-command-to-string
-   (string-join (command-list project-path symbol) " ")))
+   (string-join (add-js-import--command-list project-path symbol) " ")))
 
-(defun shell-output-to-list(output)
+(defun add-js-import--install-packages-and-rerun(project-path symbol)
+  (shell-command-to-string (concat
+                            add-js-import-npm-executable " --prefix "
+                            (file-name-directory add-js-import-node-import-path)
+                            " install "
+                            (file-name-directory add-js-import-node-import-path)))
+  (add-js-import--run-command project-path symbol))
+
+(defun add-js-import--run-command-and-error-handle(project-path symbol)
+  (let ((output (add-js-import--run-command project-path symbol)))
+    (if (string-match-p (regexp-quote "Error: Cannot find module '") output)
+        (add-js-import--install-packages-and-rerun project-path symbol)
+      output)))
+
+(defun add-js-import--shell-output-to-list(output)
   (seq-remove
-   'is-empty?
+   'add-js-import--is-empty?
    (split-string output "\n")))
 
-(defun insert-import(import)
+(defun add-js-import--insert-import(import)
   (save-excursion
     (beginning-of-buffer)
     (insert (concat import "\n"))))
 
 (defun add-js-import(project-path symbol)
   (let ((imports (shell-output-to-list
-                  (run-command
+                  (add-js-import--run-command-and-error-handle
                    project-path
                    symbol))))
     (if (eq (length imports) 1)
-        (insert-import (car imports)))))
+        (add-js-import--insert-import (car imports)))))
 
 ;;;###autoload
 (defun add-js-import-at-point()
